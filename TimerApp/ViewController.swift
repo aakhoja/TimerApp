cdc4e9579966af53ee6bae1f7ee37bea3500665e
Aman Khoja <amankhoja@tsg-sparemacb003.local>
2019-06-20 10:23:50 -0500

    Initial Commit

---
+++ TimerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by Aman Khoja on 6/20/19.
+//  Copyright © 2019 Aman Khoja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
